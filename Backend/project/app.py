44c94249265b3992f282ee3e0a43bf7bbd444424
Jemal <53106252+Jebdi@users.noreply.github.com>
2020-05-05 18:50:19 -0500

    settings

--- Backend/project/app.py
+++ Backend/project/app.py
@@ -1,1 +1,9 @@
- migrate.py Model.py requirements.tx run.py config.py 
+from flask import Blueprint
+from flask_restful import Api
+from resources.Hello import Hello
+
+api_bp = Blueprint('api', __name__)
+api = Api(api_bp)
+
+# Route
+api.add_resource(Hello, '/Hello')
