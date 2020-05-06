d8532608192a7b4ea781d0f04dc8074b0efbbf24
Jemal <53106252+Jebdi@users.noreply.github.com>
2020-05-05 19:51:03 -0500

    Create Dataabse Models

--- Backend/project/run.py
+++ Backend/project/run.py
@@ -8,7 +8,7 @@ def create_app(config_filename):
     from app import api_bp
     app.register_blueprint(api_bp, url_prefix='/api')
 
-    from Model import db
+    from Models import db
     db.init_app(app)
 
     return app
