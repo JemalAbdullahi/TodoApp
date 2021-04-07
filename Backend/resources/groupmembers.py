1c0c2f9c5b16170c35edb9241b73bdfa9bd351ff
Jemal <53106252+Jebdi@users.noreply.github.com>
2021-04-06 20:11:58 -0500

    Updated Backend

---
+++ Backend/resources/groupmembers.py
@@ -0,0 +1,104 @@
+from flask_restful import Resource
+from flask import request
+from Models import db, Task, User, Group
+
+
+
+class GroupMembers(Resource):
+
+    # List Members of a Group
+    def get(self):
+        result = []
+        header = request.headers["Authorization"]
+
+        if not header:
+            return {"Messege": "No group key!"}, 401
+        else:
+            group = Group.query.filter_by(group_key=header).first()
+            if group:
+                result = group.get_members()
+                return {"status": 'success', 'data': result}, 200
+            else:
+                return {"status": "Group Not Found"}, 404
+
+    # Add Members to a Group, only if Group is public
+    def post(self):
+        header = request.headers["Authorization"]
+        json_data = request.get_json(force=True)
+
+        if not json_data:
+            return {'Message': 'No input data provided'}, 400
+        if not header:
+            return {"Messege": "No group key!"}, 401
+        else:
+            group = Group.query.filter_by(group_key=header).first()
+            if group:
+                if group.is_public:
+                    member = User.query.filter_by(
+                        username=json_data['username']).first()
+                    if member:
+                        for m in group.members:
+                            if member.username == m.username:
+                                return {"status": "User is already added"}, 409
+                        group.members.append(member)
+                        db.session.commit()
+                        result = group.get_members()
+                        return {"status": 'success', 'data': result}, 201
+                    else:
+                        return {
+                            "status": 'No user found by that username'
+                        }, 404
+                else:
+                    return {"status": 'Group is not public'}, 403
+            else:
+                return {"Messege": "No Group Found with that group key"}, 404
+
+    """ Update Group Members: to be create if roles are implemented
+    def put(self):
+        header = request.headers["Authorization"]
+        json_data = request.get_json(force=True)
+
+        if not header:
+            return {"Messege": "No group key!"}, 400
+        else:
+            group = Group.query.filter_by(group_key=header).first()
+            if group:
+                if (group.name != json_data['name']):
+                    group.name = json_data['name']
+                #add members field
+
+                db.session.commit()
+
+                result = Group.serialize(group)
+                return {"status": 'success', 'data': result}, 201
+            else:
+                return {"Messege": "No Group with that group key"}, 402 
+    """
+
+    # Delete Members: Also, delete Member's tasks related to the specified Group
+    def delete(self):
+        header = request.headers["Authorization"]
+        username = request.args.get('username')
+
+        if not header:
+            return {"Messege": "No group key!"}, 401
+        if not username:
+            return {'Message': 'No username provided'}, 400
+        else:
+            group = Group.query.filter_by(group_key=header).first()
+            if group:
+
+                for m in group.members:
+                    if m.username == username:
+                        result = User.serialize(m)
+                        group_tasks = Task.query.filter_by(group_id=group.id)
+                        for task in group_tasks:
+                            db.session.delete(task)
+                        group.members.remove(m)
+                        if group.is_empty():
+                            db.session.delete(group)
+                        db.session.commit()
+                        return {"status": 'success', "data": result}, 200
+                return {"status": "Member Not Found in Group"}, 404
+            else:
+                return {"status": "Group Not Found"}, 404
---
+++ Backend/resources/user.py
@@ -0,0 +1,95 @@
+from flask_restful import Resource
+from flask import request
+from Models import db, User
+import random
+import string
+
+
+class Users(Resource):
+    # Get User List
+    def get(self):
+        users = User.query.all()
+        user_list = []
+        for user in users:
+            user_list.append(user.serialize_public())
+        return {"status": user_list}, 200
+
+    # Create New User (Sign Up/register User)
+    def post(self):
+        json_data = request.get_json(force=True)
+
+        if not json_data:
+            return {'Message': 'No input data provided'}, 400
+
+        user = User.query.filter_by(username=json_data['username']).first()
+        if user:
+            return {'Message': 'Username is already taken'}, 409
+
+        user = User.query.filter_by(
+            emailaddress=json_data['emailaddress']).first()
+        if user:
+            return {'Message': 'Email address already exists'}, 409
+
+        api_key = self.generate_key()
+
+        user = User.query.filter_by(api_key=api_key).first()
+        if user:
+            return {'Message': 'API key already exists'}, 409
+
+        user = User(
+            api_key=api_key,
+            firstname=json_data['firstname'],
+            lastname=json_data['lastname'],
+            phonenumber=json_data['phonenumber'],
+            avatar=json_data['avatar'],
+            emailaddress=json_data['emailaddress'],
+            password=json_data['password'],
+            username=json_data['username'],
+        )
+        db.session.add(user)
+        db.session.commit()
+
+        result = User.serialize(user)
+
+        return {"status": 'success', 'data': result}, 201
+
+    # Update User Profile
+    def put(self):
+        header = request.headers["Authorization"]
+        json_data = request.get_json(force=True)
+        if not header:
+            return {'Messege': "No API key!"}, 401
+        else:
+            user = User.query.filter_by(api_key=header).first()
+            if user:
+                if user.password != json_data["currentPassword"]:
+                    return {"Message": "Incorrect Current Password"}
+                else:
+                    if (user.username != json_data['username']):
+                        user.username = json_data['username']
+                    if (user.firstname != json_data['firstname']):
+                        user.firstname = json_data['firstname']
+                    if (user.lastname != json_data['lastname']):
+                        user.lastname = json_data['lastname']
+                    if (user.phonenumber != json_data['phonenumber']):
+                        user.phonenumber = json_data['phonenumber']
+                    if (user.avatar != json_data['avatar']):
+                        user.avatar = json_data['avatar']
+                    if (user.emailaddress != json_data['email']):
+                        user.emailaddress = json_data['email']
+                    if (user.password != json_data['newPassword']):
+                        user.password = json_data['newPassword']
+
+                db.session.commit()
+
+                result = User.serialize(user)
+                return {"status": 'success', 'data': result}, 200
+
+            else:
+                return {'Messege': "No User found with that api key"}, 404
+
+    # Generate new api key
+    def generate_key(self):
+        return ''.join(
+            random.choice(string.ascii_letters + string.digits)
+            for _ in range(50))
